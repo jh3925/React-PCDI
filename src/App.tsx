c41fce06e0bda5d0c85c19cf0780ac18e49db1c6
jh3925 <114601751+jh3925@users.noreply.github.com>
2022-09-30 23:05:08 -0400

    Minor change to App.css and App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './assets/pcdi-logo-dark.png';
 import './App.css';
 
+//This App.tsx file serves as a place to build components together. 
+//It may be placed into seperate files later.
+
 function Navbar() {
   return (
     <div className="App-navbar">
@@ -26,11 +29,12 @@ function Home() {
         <h1>About Us</h1>
         <p>
         PCDI is a tech company developing and using disruptive technology to create, enter, and innovate niche B2B and B2C markets and industries. PCDI's first target is vending machines across the US.
-        Here at PCDI we are driven to provide the world with convenience through technological disruption.</p>
+        Here at PCDI we are driven to provide the world with convenience through technological disruption.
+        </p>
       </div>
       <div>
         <h1>Our Subsidiaries</h1>
-        <p>Lula is the Future of Automated Retail. It is a marketplace for the vending machine industry connecting vendors, suppliers, and consumers.</p>
+        <p>Lula is the Future of Automated Retail. It is a marketplace for the vending machine industry connecting vendors, suppliers, and consumers."</p>
         <p>OnM is the go-to online marketplace for all prints associated with manga, comics, and similar art types. OnM provides a unique and distinct community for weebs to discover, transform, preserve, and share their passion through our customizable print options including electronics, wall decor, home goods, etc.</p>
       </div>
     </div>
