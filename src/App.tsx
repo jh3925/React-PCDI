9516cdc4f405e88f3c0d9dc63555205a03c1ead0
jh3925 <114601751+jh3925@users.noreply.github.com>
2022-10-02 22:36:23 -0400

    Add Hero, Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
 import './App.css';
+import './styles.css'
 
 import Navbar from './components/Navbar/Navbar'
 import CardContainer from './components/CardContainer/CardContainer';
 
 import LulaLogo from './assets/lula-logo-dark.png'
 import OnMLogo from './assets/onm-logo-dark.png'
+import Footer from './components/Footer/Footer';
 
 function Home() {
   return (
-    <div className="App-body">
-      <div>
-        <h1>About Us</h1>
+    <div className="home-body">
+      <div className="hero-Container">
+        <h1>Welcome to PCDI</h1>
         <p>
         PCDI is a tech company developing and using disruptive technology to create, enter, and innovate niche B2B and B2C markets and industries. PCDI's first target is vending machines across the US.
         Here at PCDI we are driven to provide the world with convenience through technological disruption.
         </p>
       </div>
-      <div>
-        <h1>Our Subsidiaries</h1>
-        <CardContainer 
+      <div className='body'>
+        <h1 id='body'>Our Subsidiaries</h1>
+        <CardContainer
           title="Ookii na Manga" 
           description="OnM is the go-to online marketplace for all prints associated with manga, comics, and similar art types. OnM provides a unique and distinct community for weebs to discover, transform, preserve, and share their passion through our customizable print options including electronics, wall decor, home goods, etc." 
           image={OnMLogo}/>
@@ -37,6 +39,7 @@ function App() {
     <div>
       <Navbar />
       <Home />
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Peddy Corporation of Disruption and Innovation Inc. | Philadelphia, PA | contact@ookiinamanga.com</p>
+        </div>
+    )
+}
+
+export default Footer;
