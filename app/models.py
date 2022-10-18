3c106e5043493c0351feeac24b1dc2e42500afab
jh3925 <114601751+jh3925@users.noreply.github.com>
2022-10-17 21:42:15 -0400

    Add models.py

---
+++ app/models.py
@@ -0,0 +1,13 @@
+import datetime as _dt
+import sqlalchemy as _sql
+
+import database as _database
+
+class User(_database.Base):
+    __tablename__ = "users"
+    id = _sql.Column(_sql.Integer, primary_key=True, index=True)
+    username = _sql.Column(_sql.String, unique=True, index=True)
+    email = _sql.Column(_sql.String, unique=True, index=True)
+    date_created = _sql.Column(_sql.DateTime, default=_dt.datetime.utcnow)
+
+    items = _sql.orm.relationship("Item", back_populates="owner")
