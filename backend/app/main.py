10a9c909f4895b53d3f327389b27a4b6410de18c
jh3925 <114601751+jh3925@users.noreply.github.com>
2022-10-19 01:36:31 -0400

    Move files

---
+++ backend/app/main.py
@@ -0,0 +1,44 @@
+#Main FastAPI app file
+from typing import Union, TYPE_CHECKING
+from fastapi import FastAPI, Depends
+import sqlalchemy.orm.session as Session
+
+from services import get_db
+from models import User
+import schemas as Schemas
+
+#Type checking is a way to tell the Python interpreter to check the types of your code.
+if TYPE_CHECKING:
+    from sqlalchemy.orm import Session
+
+app = FastAPI(title="FastAPI, Docker, OAuth2, and PostgreSQL exercise")
+
+#standard Hello World route
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+#Create a new user
+@app.post("/")
+async def create_user(user: Schemas.CreateUser, db: Session = Depends(get_db)):
+    createUser = User(username=user.username, email=user.email)
+    db.add(createUser)
+    db.commit()
+    print("User created")
+    return user
+
+#get a user by id
+@app.get("/users/id/{user_id}")
+async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    try:
+        return db.query(User).filter(User.id == user_id).first()
+    except:
+        return "User not found or Internal Server Error"
+
+#get every user in the database
+@app.get("/users/all/")
+async def get_all_users(db: Session = Depends(get_db)):
+    try:
+        return db.query(User).all()
+    except:
+        return "Internal Server Error"
