959acca99e4dad80b0cd9724e63a88931bcacb40
dg964 <114426112+dg964@users.noreply.github.com>
2022-10-19 15:27:43 -0700

    Update config.py

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,9 @@
+import os
 
+from pydantic import BaseSettings, Field
+
+
+class Settings(BaseSettings):
+    db_url: str = Field(..., env='DATABASE_URL')
+
+settings = Settings() #DEFINED SETTINGS CLASS WITH DB_URL ATTRIBUTE. CREATE INSTANCE OF SETTINGS VARIABLE == DB_URL AUTOMATICALLY LOADED.
