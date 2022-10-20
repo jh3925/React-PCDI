438b531a8b4f5c75b950186a784c083cfc694787
jh3925 <114601751+jh3925@users.noreply.github.com>
2022-10-19 22:37:36 -0400

    feat: Add rest of CRUD operations

--- backend/app/main.py
+++ backend/app/main.py
@@ -18,6 +18,9 @@ app = FastAPI(title="FastAPI, Docker, OAuth2, and PostgreSQL exercise")
 def read_root():
     return {"Hello": "World"}
 
+#CRUD OPERATIONS
+
+#Create
 #Create a new user
 @app.post("/")
 async def create_user(user: Schemas.CreateUser, db: Session = Depends(get_db)):
@@ -27,18 +30,43 @@ async def create_user(user: Schemas.CreateUser, db: Session = Depends(get_db)):
     print("User created")
     return user
 
+#Read
 #get a user by id
-@app.get("/users/id/{user_id}")
+@app.get("/api/users/id/{user_id}")
 async def get_user_by_id(user_id: int, db: Session = Depends(get_db)):
     try:
         return db.query(User).filter(User.id == user_id).first()
     except:
         return "User not found or Internal Server Error"
 
+#Update
+#Update a user by id
+@app.put("/api/users/id/{user_id}")
+async def update_user_by_id(user_id: int, user: Schemas.CreateUser, db: Session = Depends(get_db)):
+    try:
+        db.query(User).filter(User.id == user_id).update({"username": user.username, "email": user.email})
+        db.commit()
+        return "User updated"
+    except:
+        return "User not found or Internal Server Error"
+
+#Delete
+#Delete a user by id
+@app.delete("/api/users/id/{user_id}")
+async def delete_user_by_id(user_id: int, db: Session = Depends(get_db)):
+    try:
+        db.query(User).filter(User.id == user_id).delete()
+        db.commit()
+        return "User deleted"
+    except:
+        return "User not found or Internal Server Error"
+
 #get every user in the database
-@app.get("/users/all/")
+@app.get("/api/users/all/")
 async def get_all_users(db: Session = Depends(get_db)):
     try:
         return db.query(User).all()
     except:
         return "Internal Server Error"
+
+#I love GitHub Autopilot
