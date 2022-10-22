73d8b834ccf3bccb02e3054aca46c5da85349853
jh3925 <114601751+jh3925@users.noreply.github.com>
2022-10-22 16:57:48 -0400

    feat: create log in, auth.py for authentification

---
+++ backend/app/auth.py
@@ -0,0 +1,80 @@
+from fastapi import FastAPI, Depends, HTTPException, status
+import sqlalchemy.orm.session as Session
+from pydantic import BaseModel
+
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+from jose import JWTError, jwt
+from passlib.context import CryptContext
+
+import database as Database
+from database import SessionLocal, engine
+import services as Services
+from models import User
+from schemas import Token, TokenData
+from datetime import datetime, timedelta
+
+#Generate a new secret key with the following command:
+#openssl rand -hex 32
+SECRET_KEY = "SECRET_KEY"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+def get_password_hash(password: str):
+    return pwd_context.hash(password)
+
+def verify_password(plain_password: str, hashed_password: str):
+    return pwd_context.verify(plain_password, hashed_password)
+
+def authenticate_user(username: str, password: str, db: Session):
+    user = Services.get_user_by_username(username, db)
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
+def create_token(data: dict, expires_delta: timedelta | None = None):
+    to_encode = data.copy()
+    if expires_delta:
+        expire = datetime.utcnow() + expires_delta
+    else:
+        expire = datetime.utcnow() + timedelta(minutes=15)
+    to_encode.update({"exp": expire})
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm="HS256")
+    return encoded_jwt
+
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=401,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        token_data = TokenData(username=username)
+    except JWTError:
+        raise credentials_exception
+    user = Services.get_user_by_username(username=token_data.username)
+    if user is None:
+        raise credentials_exception
+    return user
+
+def login_for_access_token(form_data: OAuth2PasswordRequestForm, db: Session):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = create_token(
+        data={"sub": user.username}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
--- backend/app/main.py
+++ backend/app/main.py
@@ -1,85 +1,28 @@
 #Main FastAPI app file
 from http.client import HTTPException
 from typing import Union, TYPE_CHECKING
-from fastapi import FastAPI, Depends, HTTPException
+from fastapi import FastAPI, Depends, HTTPException, status
 import sqlalchemy.orm.session as Session
 from pydantic import BaseModel
+import database as Database
 
 import services as Services
 from models import User
 import schemas as Schemas
-from datetime import datetime, timedelta
 
+import auth as Auth
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from jose import JWTError, jwt
-from passlib.context import CryptContext
-
-SECRET_KEY = "f894fe55f80b0f9dfc01b84d26a6d88020bb0e707f1a84159e19142020efe6e5" #Example secret key, replace with your own with "openssl rand -hex 32" in bash.
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-TOKEN_SECRET = "secret"
-
-#Type checking is a way to tell the Python interpreter to check the types of your code.
-if TYPE_CHECKING:
-    from sqlalchemy.orm import Session
 
 app = FastAPI(title="FastAPI, Docker, OAuth2, and PostgreSQL exercise")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/token")
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+@app.post("/api/token", response_model=Schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(Database.get_db)):
+    return Auth.login_for_access_token(form_data, db)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def get_password_hash(password: str):
-    return pwd_context.hash(password)
-
-def verify_password(plain_password: str, hashed_password: str):
-    return pwd_context.verify(plain_password, hashed_password)
-
-"""
-DEV
-
-@app.post("/token", response_model=Schemas.Token)
-async def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(Services.get_db)):
-    user = authenticate_user(db, form_data.username)
-
-def authenticate_user(username: str, password: str,db: Session = Depends(Services.get_db)):
-    user = Services.get_user_by_username(db, username)
-    if not user:
-        return False
-    if not verify_password(password, user.hashed_password):
-        return False
-    return user
-
-def create_access_token(data: dict, expires_delta: timedelta | None = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-@app.post("/token", response_model=Schemas.Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(Services.get_db)):
-    user = authenticate_user(db, form_data.username, form_data.password)
-    if not user:
-        raise HTTPException(
-            status_code=401,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-@app.get("/items")
-async def read_items(token: str = Depends(oauth2_scheme)):
-    return {"token": token}
-"""
-
+@app.get("/users/me/")
+async def read_users_me(current_user: User = Depends(Auth.get_current_user)):
+    return current_user
 
 #standard Hello World route
 @app.get("/")
@@ -91,7 +34,7 @@ def read_root():
 #Create
 #Create a new user
 @app.post("/api/users")
-async def create_user(user: Schemas.CreateUser, db: Session = Depends(Services.get_db)):
+async def create_user(user: Schemas.CreateUser, db: Session = Depends(Database.get_db)):
     db_user = Services.get_user_by_email(email=user.email, db=db)
     if db_user != None:
         raise HTTPException(status_code=400, detail="Username already taken, or Email already registered")
@@ -103,7 +46,7 @@ async def create_user(user: Schemas.CreateUser, db: Session = Depends(Services.g
 #Read
 #get a user by id
 @app.get("/api/users/id/{user_id}")
-async def get_user_by_id(user_id: int, db: Session = Depends(Services.get_db)):
+async def get_user_by_id(user_id: int, db: Session = Depends(Database.get_db)):
     db_user = Services.get_user_by_id(user_id, db)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -111,13 +54,13 @@ async def get_user_by_id(user_id: int, db: Session = Depends(Services.get_db)):
 
 #get every user in the database
 @app.get("/api/users/all/")
-async def get_all_users(page: int = 0, limit: int = 100, db: Session = Depends(Services.get_db)):
+async def get_all_users(page: int = 0, limit: int = 100, db: Session = Depends(Database.get_db)):
     return Services.get_all_users(db, page=page, limit=limit)
 
 #Update
 #Update a user by id
 @app.put("/api/users/id/{user_id}")
-async def update_user_by_id(user_id: int, user: Schemas.CreateUser, db: Session = Depends(Services.get_db)):
+async def update_user_by_id(user_id: int, user: Schemas.CreateUser, db: Session = Depends(Database.get_db)):
     db_user = Services.get_user_by_id(user_id, db)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
@@ -126,7 +69,7 @@ async def update_user_by_id(user_id: int, user: Schemas.CreateUser, db: Session 
 #Delete
 #Delete a user by id
 @app.delete("/api/users/id/{user_id}")
-async def delete_user_by_id(user_id: int, db: Session = Depends(Services.get_db)):
+async def delete_user_by_id(user_id: int, db: Session = Depends(Database.get_db)):
     db_user = Services.get_user_by_id(user_id, db)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
